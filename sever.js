a086f5fabe63848c7c5e3d5f5e55314e5b932a1f
yeieijsjsiaia-boop <yeieijsjsiaia@gmail.com>
2026-03-05 15:09:51 +0700

    Refactor server to use environment variable for port

--- sever.js
+++ sever.js
@@ -16,6 +16,8 @@ io.on("connection", (socket) => {
 
 })
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000")
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
 })
