d16afa660b9fda78882fc299d3edde4c5ac7cd25
yeieijsjsiaia-boop <yeieijsjsiaia@gmail.com>
2026-03-05 15:16:08 +0700

    Add basic chat functionality with socket.io

---
+++ public/app.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  const item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+});
